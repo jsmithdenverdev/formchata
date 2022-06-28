ebfb97b2f28dbd418b77b3b98f1c21a250fa2abe
Jacob Smith <jsmithdenverdev@gmail.com>
2022-06-28 14:05:44 -0600

    Fixed authorizer and returning dummy user-id

--- services/auth/cmd/authorize/main.go
+++ services/auth/cmd/authorize/main.go
@@ -11,18 +11,24 @@ func main() {
 	runtime.Start(handler)
 }
 
-func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) events.APIGatewayCustomAuthorizerResponse {
+func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 	return events.APIGatewayCustomAuthorizerResponse{
 		PrincipalID: "user",
 		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
 			Version: "2012-10-17",
 			Statement: []events.IAMPolicyStatement{
 				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   "Allow",
-					Resource: []string{event.MethodArn},
+					Action: []string{"execute-api:Invoke"},
+					Effect: "Allow",
+					// TODO: this should be []string{event.MethodArn} but caching is making
+					// that difficult. The policy is cached for 300 seconds once its executed
+					// which means a user only has access to the first method they requested.
+					Resource: []string{"*"},
 				},
 			},
 		},
-	}
+		Context: map[string]interface{}{
+			"userId": "123-test",
+		},
+	}, nil
 }
